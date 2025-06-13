e563933a7400a752109932393c59e77cc21a445a
root <a234123234a@gmail.com>
2025-06-13 20:40:40 +0800

    Site updated: 2025-06-13 20:40:40

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1749424694807";const preCache = ["/images/taichi.png","/images/600211.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1749818437856";const preCache = ["/images/taichi.png","/images/600211.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
